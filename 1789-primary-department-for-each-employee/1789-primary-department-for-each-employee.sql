1e814d012067afe9aab5181afa9229aa51d7dd75
Shrikanta Ghosh <shiku6538@gmail.com>
2026-09-02 23:52:57 +0530

    Time: 637 ms (40.98%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement beloW
+(SELECT employee_id, department_id FROM Employee
+GROUP BY employee_id
+HAVING COUNT(department_id) = 1)
+UNION
+(SELECT employee_id, department_id FROM Employee
+WHERE primary_flag = "Y")
